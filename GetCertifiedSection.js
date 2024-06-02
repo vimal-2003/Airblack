4cf4bb12d64fe1a0c4637afa8de96902970b2e3d
VIMAL N <146964858+vimal-2003@users.noreply.github.com>
2024-06-02 18:22:55 +0530

    Update GetCertifiedSection.js

--- GetCertifiedSection.js
+++ GetCertifiedSection.js
@@ -1,1 +1,34 @@
+import React from 'react';
 
+const GetCertifiedSection = () => {
+  return (
+    <div style={{
+      width: 992,
+      height: 416,
+      paddingLeft: 24,
+      paddingRight: 24,
+      paddingTop: 30,
+      paddingBottom: 30,
+      background: 'black',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 14,
+      display: 'inline-flex'
+    }}>
+      {/* Your GetCertifiedSection content */}
+      <div style={{
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 25,
+        display: 'inline-flex'
+      }}>
+        {/* Inner content */}
+      </div>
+      <img style={{ width: 361, height: 252 }} src="https://via.placeholder.com/361x252" alt="placeholder" />
+    </div>
+  );
+}
+
+export default GetCertifiedSection;
