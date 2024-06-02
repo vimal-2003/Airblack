b881b8a3709bcf57d82089801d22ad1d28ffc15c
VIMAL N <146964858+vimal-2003@users.noreply.github.com>
2024-06-02 18:24:07 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import JoinAirblackSection from './components/JoinAirblackSection';
+import GetCertifiedSection from './components/GetCertifiedSection';
+import CommunitySection from './components/CommunitySection';
+
+function App() {
+  return (
+    <div className="App">
+      <JoinAirblackSection />
+      <GetCertifiedSection />
+      <CommunitySection />
+    </div>
+  );
+}
+
+export default App;
